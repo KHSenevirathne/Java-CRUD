342d5fd4dc3ceb7ea4091a60f891173fad10d131
KHSenevirathne <khsenevirathne@gmail.com>
2024-07-02 22:55:11 +0530

    add student object

---
+++ src/objects/Student.java
@@ -0,0 +1,47 @@
+package objects;
+
+public class Student {
+    private String name;
+    private String id;
+    private int age;
+    private String address;
+
+    public Student(String id, String name, int age, String address) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
